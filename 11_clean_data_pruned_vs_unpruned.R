ef17e420527bb691eedc1437923228ef6c38f325
Anthony Doan <mythicalprogrammer@gmail.com>
2018-12-20 13:03:46 -0800

    Added logic/code to aggregate all results into two files (pruned and unpruned files). Will use these two files to graph.

---
+++ 11_clean_data_pruned_vs_unpruned.R
@@ -0,0 +1,7 @@
+raw_dir_path <- "./start_data/pruned_results"
+cleaned_data_name <- "./cleaned_data/clean_pruned.csv"
+clean_result_data(raw_dir_path, cleaned_data_name)
+
+raw_dir_path <- "./start_data/no_prune_results"
+cleaned_data_name <- "./cleaned_data/clean_unpruned.csv"
+clean_result_data(raw_dir_path, cleaned_data_name)
